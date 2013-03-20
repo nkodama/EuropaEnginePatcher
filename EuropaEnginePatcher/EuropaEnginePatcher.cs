c9bec0b1431e18060e4d6d93706216aa47ad0f12
Naoki Kodama <kodama.naoki@gmail.com>
2013-03-20 15:40:59 +0900

    Ver 0.44 DDA1.2/ICoverDDAでパッチ当てに失敗する不具合修正 DDA1.2/1.3/ICoverDDAの判別処理を追加 リファクタリング
    Signed-off-by: Naoki Kodama <kodama.naoki@gmail.com>

--- EuropaEnginePatcher/EuropaEnginePatcher.cs
+++ EuropaEnginePatcher/EuropaEnginePatcher.cs
@@ -3,26 +3,26 @@ using System.Windows.Forms;
 
 namespace EuropaEnginePatcher
 {
-	/// <summary>
-	/// アプリケーションクラス
-	/// </summary>
-	public class EuropaEnginePatcher
-	{
-		/// <summary>
-		/// バージョン名
-		/// </summary>
-		internal const string VersionName = "0.43";
+    /// <summary>
+    ///     アプリケーションクラス
+    /// </summary>
+    public class EuropaEnginePatcher
+    {
+        /// <summary>
+        ///     バージョン名
+        /// </summary>
+        internal const string VersionName = "0.44";
 
-		/// <summary>
-		/// エントリーポイント
-		/// </summary>
-		[STAThread]
-		public static void Main()
-		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
+        /// <summary>
+        ///     エントリーポイント
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
 
-			Application.Run(new MainForm());
-		}
-	}
+            Application.Run(new MainForm());
+        }
+    }
 }
